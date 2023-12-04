50885437eda5186bcf9bfad8ec659b30cebcaf87
Bluemana <1xujason@gmail.com>
2023-12-04 14:41:19 +0100

    changes

--- project.py
+++ project.py
@@ -4,7 +4,7 @@ from tests import *
 from automata.fa.fa import FA
 from automata.fa.dfa import DFA
 from automata.fa.nfa import NFA
-
+from itertools import product
 from pysat.solvers import Minisat22, Minicard
 from pysat.formula import CNF, CNFPlus, IDPool
 def P_clauses(cnf, vpool, k, P):
@@ -19,17 +19,16 @@ def P_clauses(cnf, vpool, k, P):
                     nodes_list.append((start_node[0].append(i), new_alpha))
             else:
                 finished_node.append(start_node[0])
-        # all possibility for a word possibility OR possibility OR ... OR possibility
-        ORClause = []
-        for possibility in finished_node:
-            # a possibility of a word t(q0,qi,a) AND t(qi,qj,b) AND .... AND F(qk)
-            incnf = CNF()
-            for i in range(len(word)):
-                incnf.append(vpool.id(f"etat{possibility[i]}_to_etat{possibility[i+1]}_with_{word[i]}"))
-            incnf.append(vpool.id(f"etat{possibility[len(word)]}_is_final"))
-            ORClause.append(incnf)
-        # adding clause for each word
-        cnf.append(ORClause)
+    compiled_node = []
+    for possibility in finished_node:
+        poss = []
+        for i in range(len(word)):
+            poss.append(vpool.id(f"etat{possibility[i]}_to_etat{possibility[i+1]}_with_{word[i]}"))
+        poss.append(vpool.id(f"etat{possibility[len(word)]}_is_final"))
+        compiled_node.append(poss)
+    result = list(product(*compiled_node))
+    for possibility in result:
+        cnf.append(possibility)
 def N_clauses(cnf, vpool, k, N):
     for word in N:
         nodes_list = [([0],word)]
@@ -43,52 +42,43 @@ def N_clauses(cnf, vpool, k, N):
             else:
                 finished_node.append(start_node[0])
         # not possibility AND  possibility  AND ... AND not possibility
-        incnf = CNF()
         for possibility in finished_node:
             #  not t(q0,qi,a) OR not t(qi,qj,b) OR .... OR not F(qk)
             ORClause = []
             for i in range(len(word)):
                 ORClause.append(-vpool.id(f"etat{possibility[i]}_to_etat{possibility[i+1]}_with_{word[i]}"))
             ORClause.append(-vpool.id(f"etat{possibility[len(word)]}_is_final"))
-            incnf.append(ORClause)
+            cnf.append(ORClause)
         # adding clause for each word
-        cnf.append(incnf)
 def Basic_clauses(cnf, vpool, k, alphabet, P, N):
-    cnf.append(vpool.id("etat0"))
+    cnf.append([vpool.id("etat0")])
     # F(qi) -> qi
     for i in range(1, k):
         cnf.append([-vpool.id(f"etat{i}_is_final"), vpool.id(f"etat{i}")])
     # t(qi,qj,a) -> (qi AND qj)
     for i in range(k):
         for j in range(k):
-            # (AND all letter(-t(qi,qj,a) AND -t(qj,qi,a))) V (qi AND qj)
-            ORCLAUSE = []
-            # (qi and qj)
-            qandq = CNF()
-            qandq.append(vpool.id(f"etat{i}"))
-            qandq.append(vpool.id(f"etat{j}"))
-            ORCLAUSE.append(qandq)
-            # AND all letter(-t(qi,qj,a) AND -t(qj,qi,a))
-            incnf = CNF()
             for letter in alphabet:
-                incnf.append(-vpool.id(f"etat{i}_to_etat{j}_with_{letter}"))
-                incnf.append(-vpool.id(f"etat{j}_to_etat{i}_with_{letter}"))
-            ORCLAUSE.append(incnf)
-            cnf.append(ORCLAUSE)
+                # (AND all letter(-t(qi,qj,a) AND -t(qj,qi,a))) V (qi AND qj)
+                ORCLAUSE = []
+                # (qi and qj)
+                ORCLAUSE.append(vpool.id(f"etat{i}"))
+                ORCLAUSE.append(-vpool.id(f"etat{i}_to_etat{j}_with_{letter}"))
+                cnf.append(ORCLAUSE)
+                ORCLAUSE.clear()
+                ORCLAUSE.append(vpool.id(f"etat{j}"))
+                ORCLAUSE.append(-vpool.id(f"etat{i}_to_etat{j}_with_{letter}"))
+                cnf.append(ORCLAUSE)
     # t(qi,qj,a) -> (AND all k!=j -t(qi,qk,a))
     for i in range(k):
         for j in range(k):
             for letter in alphabet:
-                # -t(qi,qj,a) V (AND all k!=j -t(qi,qk,a))
-                ORCLAUSE = []
-                # -t(qi,qj,a)
-                ORCLAUSE.append(-vpool.id(f"etat{i}_to_etat{j}_with_{letter}"))
-                # (AND all k!=j -t(qi,qk,a))
-                outcnf = CNF()
                 for l in range(k):
                     if l != j:
-                        outcnf.append(-vpool.id(f"etat{i}_to_etat{l}_with_{letter}"))
-                ORCLAUSE.append(outcnf)
+                        ORCLAUSE = []
+                        ORCLAUSE.append(-vpool.id(f"etat{i}_to_etat{j}_with_{letter}"))
+                        ORCLAUSE.append(-vpool.id(f"etat{i}_to_etat{l}_with_{letter}"))
+                        cnf.append(ORCLAUSE)
     P_clauses(cnf, vpool, k, P)
     N_clauses(cnf, vpool, k, N)
 def create_automate(model, vpool, alphabet, k):
@@ -184,6 +174,17 @@ def main():
     test_autr()
     test_autcard()
     test_autn()
+def f():
+    cnf = CNF()
+    incnf = CNF()
+    incnf.append([1,2,3])
+    outcnf = CNF()
+    outcnf.append([4,5,6])
+    ORclause = []
+    ORclause.append(incnf)
+    ORclause.append(outcnf)
+    # ((a AND b) or (c AND d)) and ((a AND b) or (c AND d))
+    cnf.append(ORclause)
 
 if __name__ == '__main__':
-    main()
+    f()
